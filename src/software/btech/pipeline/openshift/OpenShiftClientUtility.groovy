d3136524e294dce383b070a8408723435d292647
Bruno Sant'Anna <bruno@btech.software>
2020-02-19 20:04:18 +0100

    OpenShiftClientUtility build command (3)

--- src/software/btech/pipeline/openshift/OpenShiftClientUtility.groovy
+++ src/software/btech/pipeline/openshift/OpenShiftClientUtility.groovy
@@ -3,12 +3,16 @@ package software.btech.pipeline.openshift
 
 import software.btech.pipeline.AbstractPipelineUtility
 
+import java.text.SimpleDateFormat
+
 /**
  * OpenShift client setup utility
  */
 class OpenShiftClientUtility extends AbstractPipelineUtility {
 
   final String clusterName;
+
+  final SimpleDateFormat dateFormat
 
   /**
    * Constructor with pipeline reference injection.
@@ -18,23 +22,58 @@ class OpenShiftClientUtility extends AbstractPipelineUtility {
   OpenShiftClientUtility(Script pipeline, String clusterName) {
     super(pipeline)
     this.clusterName = clusterName
+    this.dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
   }
 
   /**
    * Builds a project from OpenShift Cluster
    * @param projectName
+   * @param timeout for exiting
    * @return
    */
   Void buildProject(String projectName, Integer timeout) {
-    this.pipeline.openshift.withCluster(this.clusterName) {
-      this.pipeline.openshift.withProject(projectName) {
+    this.pipeline.timeout(timeout) {
+      this.pipeline.openshift.withCluster(this.clusterName) {
+        this.pipeline.openshift.withProject(projectName) {
 
-        def builds = this.pipeline.openshift.selector('bc')
-        builds.withEach {
-          it.startBuild()
+          long startTimestamp = Calendar.getInstance().getTimeInMillis()
+          int startedBuildCount = 0
+          this.pipeline.openshift.selector('bc').withEach {
+            it.startBuild()
+            startedBuildCount++
+          }
+
+          this.print(String.format("Started %s build jobs", startedBuildCount))
+
+          boolean isComplete = false
+          while (!isComplete) {
+
+            def buildList = this.pipeline.openshift.selector("build").objects()
+            def completeBuilds = []
+            for (Object build : buildList) {
+              long buildStartTimestamp = this.dateFormat.parse(build.status.startTimestamp).getTime()
+              if (buildStartTimestamp > startTimestamp) {
+                if ("Complete".equalsIgnoreCase(build.status.phase)) {
+                  completeBuilds.add(build)
+                }
+              }
+            }
+
+            // loop condition
+            isComplete = completeBuilds.size() == startedBuildCount
+            if (!isComplete) {
+              int sleepTime = 5
+              String unit = "MINUTES"
+              this.print(String.format("Sleeping for %s %s", timeout, unit.toLowerCase()))
+              this.pipeline.sleep(time: sleepTime, unit: unit)
+            } else {
+              this.print("Build Complete")
+              for (Object build : completeBuilds) {
+                build.describe()
+              }
+            }
+          }
         }
-
-        this.pipeline.sleep(time: timeout, unit: "MINUTES")
       }
     }
   }
